f1bbddb3425cb034b3e0d026bd9fd8bc3605f8b2
Chris Carter <chris@ccarteronline.com>
2015-07-02 00:27:44 -0500

    fix?

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-//configure app to use body parser
-//this will allow us to get data from post
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
